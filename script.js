937f9df4ebf55326b0e76ef89cdd04114898f22a
Ramiro Severgnini <160265951+ramirosevergnini@users.noreply.github.com>
2024-05-09 15:18:02 -0300

    página do chatbot quase pronta

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
 
-// classList - shows/gets all classes
+
 const navToggle = document.querySelector('.nav-toggle');
 const links = document.querySelector('.links');
 
-// contains - checks classList for specific class
-// add - add class
-// remove - remove class
-// toggle - toggles class
 
 navToggle.addEventListener('click', () => {
     console.log(links.classList);
@@ -18,3 +14,88 @@ navToggle.addEventListener('click', () => {
     }
 
 });
+
+
+const apiKey = 'AIzaSyBC6O7pGbjPwZ-bx8Ko6Uy9pcHi_HL67d4'
+
+function sendMessage(){
+    var message = document.getElementById('message-input')
+    if(!message.value)
+    {
+        message.style.border = '1px solid red'
+        return;
+    }
+    message.style.border = 'none'
+
+    var status = document.getElementById('status')
+    var btnSubmit = document.getElementById('btn-submit')
+
+    status.style.display = 'block'
+    status.innerHTML = 'Carregando...'
+    btnSubmit.disabled = true
+    btnSubmit.style.cursor = 'not-allowed'
+    message.disabled = true
+
+    fetch("https://api.openai.com/v1/completions",{
+        method: 'POST',
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${apiKey}`,
+        },
+        body: JSON.stringify({
+            model: "text-davinci-003",
+            prompt: message.value,
+            max_tokens: 2048, // tamanho da resposta
+            temperature: 0.5 // criatividade na resposta
+        })
+    })
+    .then((response) => response.json())
+    .then((response) => {
+        let r = response.choices[0]['text']
+        status.style.display = 'none'
+        showHistory(message.value,r)
+    })
+    .catch((e) => {
+        console.log(`Error -> ${e}`)
+        status.innerHTML = 'Erro, tente novamente mais tarde...'
+    })
+    .finally(() => {
+        btnSubmit.disabled = false
+        btnSubmit.style.cursor = 'pointer'
+        message.disabled = false
+        message.value = ''
+    })
+}
+
+function showHistory(message,response){
+    var historyBox = document.getElementById('history')
+
+    // My message
+    var boxMyMessage = document.createElement('div')
+    boxMyMessage.className = 'box-my-message'
+
+    var myMessage = document.createElement('p')
+    myMessage.className = 'my-message'
+    myMessage.innerHTML = message
+
+    boxMyMessage.appendChild(myMessage)
+
+    historyBox.appendChild(boxMyMessage)
+
+    // Response message
+    var boxResponseMessage = document.createElement('div')
+    boxResponseMessage.className = 'box-response-message'
+
+    var chatResponse = document.createElement('p')
+    chatResponse.className = 'response-message'
+    chatResponse.innerHTML = response
+
+    boxResponseMessage.appendChild(chatResponse)
+
+    historyBox.appendChild(boxResponseMessage)
+
+    // Levar scroll para o final
+    historyBox.scrollTop = historyBox.scrollHeight
+}
+
